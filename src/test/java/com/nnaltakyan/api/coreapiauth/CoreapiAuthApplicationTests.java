525d04c6cf19ef718d6238a7566a676dfe483d0f
Narek Naltakyan <nareknaltakyan1@gmail.com>
2024-04-18 12:35:27 +0400

    initial commit

---
+++ src/test/java/com/nnaltakyan/api/coreapiauth/CoreapiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnaltakyan.api.coreapiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreapiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
