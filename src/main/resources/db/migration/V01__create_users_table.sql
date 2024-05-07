e10311aec52266dac12411a5455636db55e7144f
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-05-08 00:14:57 +0400

    Refactoring and fixing the issue

--- src/main/resources/db/migration/V01__create_users_table.sql
+++ src/main/resources/db/migration/V01__create_users_table.sql
@@ -1,11 +1,8 @@
 CREATE TABLE _user (
     id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) NOT NULL CHECK (role IN ('ROLE_USER', 'ROLE_ADMIN')),
-    is_account_non_expired BOOLEAN NOT NULL DEFAULT TRUE,
-    is_account_non_locked BOOLEAN NOT NULL DEFAULT TRUE,
-    is_credentials_non_expired BOOLEAN NOT NULL DEFAULT TRUE
+    role VARCHAR(20) NOT NULL CHECK (role IN ('USER', 'ADMIN'))
 );
