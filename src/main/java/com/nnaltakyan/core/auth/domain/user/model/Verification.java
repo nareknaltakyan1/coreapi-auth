70c5b7e1d8a8eb24a1bf8034c1815c14c33cc7a9
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-05-13 23:25:41 +0400

    Draft changes

--- src/main/java/com/nnaltakyan/core/auth/domain/user/model/Verification.java
+++ src/main/java/com/nnaltakyan/core/auth/domain/user/model/Verification.java
@@ -1,20 +1,21 @@
-//package com.nnaltakyan.core.auth.domain.user.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-//@Table(name = "_verifiction")
-//public class Verification {
-//    private String email;
-//    private String otp;
-//    private boolean verified;
-//}
+package com.nnaltakyan.core.auth.domain.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_verifiction")
+public class Verification {
+    private String user_id;
+    private String verification_code;
+    private boolean created_time;
+    private boolean verified;
+}
