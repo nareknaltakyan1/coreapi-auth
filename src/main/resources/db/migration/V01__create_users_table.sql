a75de8790c47720ed0e3713ace4f5cab6a00f782
asgrigoryan <148821211+asgrigoryan@users.noreply.github.com>
2024-05-13 11:16:22 +0400

    Update V01__create_users_table.sql
    Refactoring sql scripts

--- src/main/resources/db/migration/V01__create_users_table.sql
+++ src/main/resources/db/migration/V01__create_users_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE "_user" (
-    id BIGSERIAL,
+    id BIGSERIAL NOT NULL,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
     created TIMESTAMP NOT NULL,
-    deleted TIMESTAMP NOT NULL,
     updated TIMESTAMP NOT NULL,
+    deleted TIMESTAMP,
     PRIMARY KEY(id),
-    CONSTRAINT nn_email_unique UNIQUE (email)
+    CONSTRAINT uq_email UNIQUE (email)
 );
 
 CREATE INDEX idx_user_email ON "_user" (email);
